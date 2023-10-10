53a1de24a4abda03f4f0c447de426a711693a613
rupakkarki27 <rupakkarki123@gmail.com>
2023-10-10 13:00:36 +0545

    server (chore): add new tests

--- apps/server/src/modules/allergy/dto/update-allergy.dto.ts
+++ apps/server/src/modules/allergy/dto/update-allergy.dto.ts
@@ -1,23 +1,28 @@
 import { AllergySeverity } from '@allergy-management/models';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
 
 export class UpdateAllergyDto {
   @ApiPropertyOptional()
+  @IsOptional()
   name?: string;
 
   @ApiPropertyOptional({ isArray: true, type: String })
+  @IsOptional()
   @Transform(({ value }) => (Array.isArray(value) ? value : Array(value)))
   symptoms?: string[];
 
   @ApiPropertyOptional()
+  @IsOptional()
   @IsEnum(AllergySeverity)
   severity?: AllergySeverity;
 
   @ApiPropertyOptional({ type: Boolean })
+  @IsOptional()
   isHighRisk?: boolean;
 
   @ApiPropertyOptional()
+  @IsOptional()
   notes?: string;
 }
